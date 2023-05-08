58b8edbccd29924c82082eaadf5458c78ecec9e5
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-05-08 13:11:10 +0200

    display current build date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'swimresults';
+
+  build: string = "";
+  showBuild: boolean = true;
+
+  constructor() {
+    this.fetchBuild().then(r => {
+      this.build = r;
+    });
+  }
+
+  async fetchBuild() {
+    const response = await fetch("assets/release.txt");
+    return await response.text();
+  }
+
+  toggleBuild() {
+    this.showBuild = !this.showBuild;
+  }
 }
