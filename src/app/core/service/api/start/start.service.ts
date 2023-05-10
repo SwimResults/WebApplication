f395f2e0e31e9b88480bba0e42d893bdc0b6cb59
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-05-11 00:03:14 +0200

    start service

---
+++ src/app/core/service/api/start/start.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from "../base.service";
+import {environment} from "../../../../../environments/environment";
+import {Observable} from "rxjs";
+import {Start} from "../../../model/start/start.model";
+import {ApiService} from "../api.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StartService extends BaseService {
+
+  constructor(
+    private apiService: ApiService
+  ) {
+    super("StartService", environment.api_urls.start_service);
+  }
+
+  getStartsByMeeting(id: string): Observable<Start[]> {
+    return this.apiService.get(this.API_URL, "start/meet/" + id);
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,9 @@
 export const environment = {
   api_urls: {
     athlete_service: "https://api.swimresults.de/athlete/v1/",
+    start_service: "https://api.swimresults.de/start/v1/",
+    import_service: "https://api.swimresults.de/import/v1/",
+    user_service: "https://api.swimresults.de/user/v1/",
     meeting_service: "https://api.swimresults.de/meeting/v1/"
   }
 }
