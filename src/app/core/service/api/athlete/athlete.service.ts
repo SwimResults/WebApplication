3225ccade49e1b789e236b4e2129826a6592a06a
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-05-11 20:40:05 +0200

    move athlete service; get athletes by meeting request;

--- src/app/core/service/api/athlete/athlete.service.ts
+++ src/app/core/service/api/athlete/athlete.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import {BaseService} from "./base.service";
-import {ApiService} from "./api.service";
+import {BaseService} from "../base.service";
+import {ApiService} from "../api.service";
 import {Observable} from "rxjs";
-import {Athlete} from "../../model";
-import {environment} from "../../../../environments/environment";
+import {Athlete} from "../../../model";
+import {environment} from "../../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +23,9 @@ export class AthleteService extends BaseService{
 
   public getAthleteById(id: string): Observable<Athlete> {
     return this.apiService.get(this.API_URL, "athlete/" + id);
+  }
+
+  public getAthletesByMeeting(id: string): Observable<Athlete[]> {
+    return this.apiService.get(this.API_URL, "athlete/meet/" + id);
   }
 }
