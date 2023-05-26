d4f547adaebac5b730d0fdcc3e86fb3576b830f7
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-05-26 13:49:49 +0200

    init start page component

--- src/app/content/starts/starts.module.ts
+++ src/app/content/starts/starts.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StartListTileComponent } from './components';
+import { StartListTileComponent, StartListComponent } from './components';
 import {ElementsModule} from "../../shared/elements/elements.module";
 import {RouterLink} from "@angular/router";
-import { StartListComponent } from './components/start-list/start-list.component';
+import { PageStartComponent } from './pages';
 import {CoreModule} from "../../core/core.module";
-
 
 
 @NgModule({
     declarations: [
         StartListTileComponent,
-        StartListComponent
+        StartListComponent,
+        PageStartComponent
     ],
     exports: [
         StartListTileComponent,
