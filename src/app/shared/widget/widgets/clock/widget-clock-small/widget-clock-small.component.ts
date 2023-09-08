a1038357999f257e1caba26209c31f549156ff28
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-09-08 20:48:43 +0200

    clock small widget use seconds

--- src/app/shared/widget/widgets/clock/widget-clock-small/widget-clock-small.component.ts
+++ src/app/shared/widget/widgets/clock/widget-clock-small/widget-clock-small.component.ts
@@ -7,18 +7,32 @@ import {Component, OnInit} from '@angular/core';
 })
 export class WidgetClockSmallComponent implements OnInit {
 
-  currentTime: string = "HH:mm";
+  clockUpdateInterval: number = 1000;
+
+
+  currentTime: string = "HH:mm:ss";
+  hours: string = "HH";
+  minutes: string = "mm";
+  seconds: string = "ss";
   currentDate: string = "dd.MM.YYYY";
   time: Date = new Date();
 
+  private interval: any;
+
   ngOnInit(): void {
     this.fetchTime();
+
+    this.interval = setInterval(() => {
+      this.fetchTime();
+    }, this.clockUpdateInterval);
   }
 
   fetchTime(): void {
-    console.log("tick")
     this.time = new Date();
-    this.currentTime = ("0" + this.time.getHours()).slice(-2) + ":" + ("0" + this.time.getMinutes()).slice(-2);
+    this.hours = ("0" + this.time.getHours()).slice(-2);
+    this.minutes = ("0" + this.time.getMinutes()).slice(-2);
+    this.seconds = ("0" + this.time.getSeconds()).slice(-2);
+    this.currentTime = this.hours + ":" + this.minutes + ":" + this.seconds;
     this.currentDate = this.time.getDate() + "." + (this.time.getMonth() + 1) + "." + this.time.getFullYear();
   }
 
