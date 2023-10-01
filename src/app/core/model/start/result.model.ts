1118c2d6e5e0eb64f0e3bac2c04c48e8333daa79
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-10-01 12:47:32 +0300

    livetiming show lap times

--- src/app/core/model/start/result.model.ts
+++ src/app/core/model/start/result.model.ts
@@ -1,6 +1,7 @@
 
 export interface Result {
   result_type: string;
+  lap_meters: number;
   time: number;
   added_at: string;
 }
@@ -8,6 +9,7 @@ export interface Result {
 export class ResultImpl implements Result {
   added_at: string;
   result_type: string;
+  lap_meters: number;
   time: number;
   addedAtTime: Date
 
@@ -15,6 +17,7 @@ export class ResultImpl implements Result {
   constructor(result: Result) {
     this.added_at = result.added_at;
     this.result_type = result.result_type;
+    this.lap_meters = result.lap_meters;
     this.time = result.time;
     this.addedAtTime = new Date(result.added_at)
   }
