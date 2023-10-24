6e2c17353020057815c72757e89b054275775b40
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-10-24 14:37:37 +0200

    admin live heat load starts, event, enter event and heat + control buttons

--- src/app/content/admin/admin.module.ts
+++ src/app/content/admin/admin.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageAdminEventComponent } from './pages';
 import { AdminMainViewComponent, AdminHeatToolComponent, AdminImportToolComponent, AdminEventListToolComponent } from './components';
+import {ElementsModule} from "../../shared/elements/elements.module";
+import {EventsModule} from "../events";
+import {CoreModule} from "../../core/core.module";
+import {StartsModule} from "../starts";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatIconModule} from "@angular/material/icon";
 
 
 
@@ -14,7 +20,13 @@ import { AdminMainViewComponent, AdminHeatToolComponent, AdminImportToolComponen
     AdminEventListToolComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ElementsModule,
+    EventsModule,
+    CoreModule,
+    StartsModule,
+    ReactiveFormsModule,
+    MatIconModule
   ]
 })
 export class AdminModule { }
--- src/app/content/admin/components/admin-heat-tool/admin-heat-tool.component.ts
+++ src/app/content/admin/components/admin-heat-tool/admin-heat-tool.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Start} from "../../../../core/model/start/start.model";
+import {HeatImpl} from "../../../../core/model/start/heat.model";
+import {FetchingModel} from "../../../../core/model/common/fetching.model";
+import {StartListTileConfig} from "../../../../core/model/start/start-list-tile-config.model";
+import {EventService, StartService} from "../../../../core/service/api";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {MeetingEvent} from "../../../../core/model/meeting/meeting-event.model";
 
 @Component({
   selector: 'sr-admin-heat-tool',
   templateUrl: './admin-heat-tool.component.html',
   styleUrls: ['./admin-heat-tool.component.scss']
 })
-export class AdminHeatToolComponent {
+export class AdminHeatToolComponent implements OnInit {
+  @Input() meetingId?: string;
+  heat?: HeatImpl;
+  starts: Start[] = [];
 
+  maxHeats: number = 100;
+
+  event: MeetingEvent = {} as MeetingEvent;
+
+  currentEvent: number = 1;
+  currentHeat: number = 1;
+
+  fetchingStarts: FetchingModel = {fetching: false};
+  config: StartListTileConfig = {
+    showAthlete: true,
+    laneAsIcon: true,
+    flatStyle: true,
+    rankStylesIcon: false,
+    showRegistrationTime: true,
+    showResults: true,
+    showResultTime: true,
+    showReactionTime: true,
+    showLapTimes: true
+  } as StartListTileConfig;
+
+  heatForm: FormGroup;
+
+  constructor(
+    private startService: StartService,
+    private fb: FormBuilder,
+    private eventService: EventService
+  ) {
+    this.heatForm = this.fb.group({
+      event: [],
+      heat: []
+    })
+  }
+
+  ngOnInit() {
+    this.fetchStarts();
+    this.fetchEvent();
+  }
+
+  fetchStarts() {
+    if (!this.meetingId) return;
+    this.fetchingStarts.fetching = true;
+    this.startService.getStartsByMeetingAndEventAndHeat(this.meetingId, this.currentEvent, this.currentHeat).subscribe(data => {
+      console.log("fetched:")
+      console.log(data)
+      if (data) {
+        if (data.length > 0) {
+          this.heat = new HeatImpl(data[0].heat);
+        }
+        this.starts = data;
+      }
+      this.fetchingStarts.fetching = false;
+    });
+  }
+
+  fetchEvent() {
+    if (this.meetingId && this.currentEvent) {
+      this.eventService.getCachedEventByMeetingAndNumber(this.meetingId, this.currentEvent).subscribe(data => {
+        this.event = data;
+      })
+    }
+  }
+
+  setHeat() {
+    this.currentEvent = this.heatForm.value.event;
+    this.currentHeat = this.heatForm.value.heat;
+    this.fetchStarts();
+    this.fetchEvent();
+  }
 }
