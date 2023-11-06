f1fa4fe46681d1062b983d740a23e669883642af
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-11-06 13:10:55 +0100

    fix tests

--- src/app/core/service/api/import/import-file.service.spec.ts
+++ src/app/core/service/api/import/import-file.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ImportFileService } from './import-file.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('ImportFileService', () => {
   let service: ImportFileService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(ImportFileService);
   });
 
