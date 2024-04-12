bda4d92684a925f77cb98b149155f84d66b6475c
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-04-12 02:48:52 +0200

    prepare for file list force download

--- src/app/core/constant/file-types.constant.ts
+++ src/app/core/constant/file-types.constant.ts
@@ -1,31 +1,38 @@
 export interface FileType {
   icon: string;
   color: string;
+  download: boolean;
 }
 
 export let fileTypes = new Map<string, FileType>([
-  ["pdf", {
-    icon: "description",
-    color: "pdf"
-  }],
-  ["dsv6", {
-    icon: "article",
-    color: "dsv"
-  }],
-  ["dsv7", {
-    icon: "article",
-    color: "dsv"
-  }],
-  ["zip", {
-    icon: "folder_zip",
-    color: "zip"
-  }],
-  ["lef", {
-    icon: "article",
-    color: "lef"
-  }],
-  ["lxf", {
-    icon: "folder_zip",
-    color: "lef"
-  }],
+    ["pdf", {
+        icon: "description",
+        color: "pdf",
+        download: false
+    }],
+    ["dsv6", {
+        icon: "article",
+        color: "dsv",
+        download: true
+    }],
+    ["dsv7", {
+        icon: "article",
+        color: "dsv",
+        download: true
+    }],
+    ["zip", {
+        icon: "folder_zip",
+        color: "zip",
+        download: true
+    }],
+    ["lef", {
+        icon: "article",
+        color: "lef",
+        download: true
+    }],
+    ["lxf", {
+        icon: "folder_zip",
+        color: "lef",
+        download: true
+    }],
 ]);
--- src/app/shared/elements/file-icon/file-icon.component.ts
+++ src/app/shared/elements/file-icon/file-icon.component.ts
@@ -2,41 +2,42 @@ import {Component, Input, OnInit} from '@angular/core';
 import {FileType, fileTypes} from "../../../core/constant/file-types.constant";
 
 @Component({
-  selector: 'sr-file-icon',
-  templateUrl: './file-icon.component.html',
-  styleUrls: ['./file-icon.component.scss']
+    selector: 'sr-file-icon',
+    templateUrl: './file-icon.component.html',
+    styleUrls: ['./file-icon.component.scss']
 })
 export class FileIconComponent implements OnInit {
 
-  @Input() extension!: string;
-  @Input() icon?: string;
-  @Input() existing: boolean = false;
+    @Input() extension!: string;
+    @Input() icon?: string;
+    @Input() existing: boolean = false;
 
-  fileType: FileType = {
-    icon: "insert_drive_file",
-    color: ""
-  }
-
-  constructor() {
-
-  }
-
-  ngOnInit() {
-    if (fileTypes.has(this.extension)) {
-      const ft = fileTypes.get(this.extension);
-
-      if (ft)
-        this.fileType = ft;
+    fileType: FileType = {
+        icon: "insert_drive_file",
+        color: "",
+        download: true
     }
-  }
 
-  getIcon() {
-    if (this.icon) return this.icon;
-    return this.fileType.icon;
-  }
+    constructor() {
 
-  getStyle() {
-    return this.existing ? this.fileType.color : 'inactive';
-  }
+    }
+
+    ngOnInit() {
+        if (fileTypes.has(this.extension)) {
+            const ft = fileTypes.get(this.extension);
+
+            if (ft)
+                this.fileType = ft;
+        }
+    }
+
+    getIcon() {
+        if (this.icon) return this.icon;
+        return this.fileType.icon;
+    }
+
+    getStyle() {
+        return this.existing ? this.fileType.color : 'inactive';
+    }
 
 }
