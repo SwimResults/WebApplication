7df545aefe41ec831f13022625b906034f919671
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-04-12 23:54:36 +0200

    fix speaker dashboard widget formatting

--- src/app/content/dashboard/components/speaker-dashboard/speaker-dashboard.component.ts
+++ src/app/content/dashboard/components/speaker-dashboard/speaker-dashboard.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import {Widget} from "../../../../core/model/user/widget.model";
 import {WidgetContainer} from "../../../../core/model/user/widget-container.model";
-import {WidgetTile} from "../../../../core/model/user/widget-tile.model";
 
 @Component({
   selector: 'sr-speaker-dashboard',
@@ -9,46 +8,59 @@ import {WidgetTile} from "../../../../core/model/user/widget-tile.model";
   styleUrl: './speaker-dashboard.component.scss'
 })
 export class SpeakerDashboardComponent {
-    delayWidget: Widget = {
-        _id: "",
-        content: "delay",
-        size: "SMALL"
+    widgetContainers: WidgetContainer[] = [];
+
+    constructor() {
+        let delayWidget: Widget = {
+            _id: "",
+            content: "delay",
+            size: "SMALL"
+        }
+        let clockWidget: Widget = {
+            _id: "",
+            content: "clock",
+            size: "MEDIUM"
+        }
+        let announcementWidget: Widget = {
+            _id: "",
+            content: "file-announcement",
+            size: "SMALL"
+        }
+        let startListWidget: Widget = {
+            _id: "",
+            content: "file-start-list",
+            size: "SMALL"
+        }
+
+        let widgetContainer1: WidgetContainer = {
+            order_position: 1,
+            widgets: [
+                {
+                    order_position: 1,
+                    widget: clockWidget
+                }
+            ]
+        } as WidgetContainer
+
+        let widgetContainer2: WidgetContainer = {
+            order_position: 2,
+            widgets: [
+                { order_position: 1, widget: announcementWidget },
+                { order_position: 2, widget: startListWidget }
+            ]
+        } as WidgetContainer;
+
+        let widgetContainer3: WidgetContainer = {
+            order_position: 3,
+            widgets: [
+                { order_position: 1, widget: delayWidget }
+            ]
+        } as WidgetContainer;
+
+        this.widgetContainers.push(widgetContainer1);
+        this.widgetContainers.push(widgetContainer2);
+        this.widgetContainers.push(widgetContainer3);
     }
-    clockWidget: Widget = {
-        _id: "",
-        content: "clock",
-        size: "MEDIUM"
-    }
-    announcementWidget: Widget = {
-        _id: "",
-        content: "file-announcement",
-        size: "SMALL"
-    }
-    startListWidget: Widget = {
-        _id: "",
-        content: "file-start-list",
-        size: "SMALL"
-    }
-    widgets: WidgetContainer = {
-        order_position: 1,
-        widgets: [
-            {
-                order_position: 1,
-                widget: this.clockWidget
-            },
-            {
-                order_position: 2,
-                widget: this.delayWidget
-            },
-            {
-                order_position: 3,
-                widget: this.announcementWidget
-            },
-            {
-                order_position: 4,
-                widget: this.startListWidget
-            }
-        ]
-    } as WidgetContainer;
+
 
 }
