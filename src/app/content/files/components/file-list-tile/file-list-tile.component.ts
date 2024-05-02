836e44ad27a0198504c3d8f606625126d22cd75d
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-05-02 22:53:14 +0200

    fix file list file name not in translations

--- src/app/content/files/components/file-list-tile/file-list-tile.component.ts
+++ src/app/content/files/components/file-list-tile/file-list-tile.component.ts
@@ -4,44 +4,44 @@ import {TranslateService} from "@ngx-translate/core";
 import {fileTypes} from "../../../../core/constant/file-types.constant";
 
 @Component({
-  selector: 'sr-file-list-tile',
-  templateUrl: './file-list-tile.component.html',
-  styleUrls: ['./file-list-tile.component.scss']
+    selector: 'sr-file-list-tile',
+    templateUrl: './file-list-tile.component.html',
+    styleUrls: ['./file-list-tile.component.scss']
 })
 export class FileListTileComponent implements OnInit {
 
-  @Input() file!: StorageFile
+    @Input() file!: StorageFile
 
-  name: string = "";
+    name: string = "";
 
-  getUrl() {
-    if (!this.file.existing) return "";
-    let url = this.file.url;
-    if (!this.file.url) url = "https://download.swimresults.de" + this.file.path;
-    return url;
-  }
-
-  constructor(
-    private translateService: TranslateService
-  ) {
-  }
-
-  ngOnInit() {
-    if (this.file && this.file.name) {
-      this.translateService.get("FILES.FILE_NAMES." + this.file.name.toUpperCase()).subscribe(results => {
-        if (results) {
-          this.name = results;
-        } else {
-            this.name = this.file.name;
-        }
-      })
+    getUrl() {
+        if (!this.file.existing) return "";
+        let url = this.file.url;
+        if (!this.file.url) url = "https://download.swimresults.de" + this.file.path;
+        return url;
     }
-  }
 
-  clickDownload() {
-    if (this.file.url) return;
-    this.file.downloads++;
-  }
+    constructor(
+        private translateService: TranslateService
+    ) {
+    }
+
+    ngOnInit() {
+        if (this.file && this.file.name) {
+            this.translateService.get("FILES.FILE_NAMES." + this.file.name.toUpperCase()).subscribe(results => {
+                if (results.includes("FILES.FILE_NAMES.")) {
+                    this.name = this.file.name;
+                } else {
+                    this.name = results;
+                }
+            })
+        }
+    }
+
+    clickDownload() {
+        if (this.file.url) return;
+        this.file.downloads++;
+    }
 
     protected readonly fileTypes = fileTypes;
 }
