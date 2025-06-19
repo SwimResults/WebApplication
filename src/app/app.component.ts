298da57f4ee80695521b565453ec5df430cf7c24
konrad2002 <konrad@schwimmteamerzgebirge.de>
2025-06-19 22:35:14 +0200

    add app title and description on sub pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import {HeaderComponent} from './shared/layout/header/header.component';
 import {RouterOutlet} from '@angular/router';
 import {LiveBarComponent} from './shared/layout/live-bar/live-bar.component';
 import {UpperCasePipe} from '@angular/common';
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
     selector: 'sr-root',
@@ -18,87 +19,108 @@ import {UpperCasePipe} from '@angular/common';
     imports: [SidebarComponent, HeaderComponent, RouterOutlet, LiveBarComponent, UpperCasePipe]
 })
 export class AppComponent implements OnDestroy {
-  meeting?: MeetingImpl;
-  meetingId?: string;
-  meetingSubscription: Subscription;
-  meetingIdSubscription: Subscription;
+    meeting?: MeetingImpl;
+    meetingId?: string;
+    meetingSubscription: Subscription;
+    meetingIdSubscription: Subscription;
 
-  title = 'swimresults';
+    title = 'swimresults';
 
-  build: string = "";
-  showBuild: boolean = true;
-  showEnv: boolean = true;
-  sidebarState = "";
+    build: string = "";
+    showBuild: boolean = true;
+    showEnv: boolean = true;
+    sidebarState = "";
 
-  environment = environment.environment;
-  env_color = environment.env_color;
+    environment = environment.environment;
+    env_color = environment.env_color;
 
-  constructor(
-    private translateService: TranslateService,
-    private menuService: SidebarMenuService,
-    private routeService: RouteService
-  ) {
-    this.showBuild = this.environment != 'productive'
-    this.showEnv = this.environment != 'productive'
+    constructor(
+        private translateService: TranslateService,
+        private menuService: SidebarMenuService,
+        private routeService: RouteService,
+        private headerTitle: Title,
+        private headerMeta: Meta
+    ) {
+        this.showBuild = this.environment != 'productive'
+        this.showEnv = this.environment != 'productive'
 
-    this.fetchBuild().then(r => {
-      this.build = r;
-    });
+        this.fetchBuild().then(r => {
+            this.build = r;
+        });
 
-    let lang = window.localStorage.getItem("language");
-    if (!lang) {
-        //lang = navigator.language;
-        lang = "de";
-    }
-    this.translateService.use(lang);
-
-    this.menuService.viewType.subscribe(data => {
-      this.sidebarState = data;
-    })
-
-
-    this.meetingSubscription = this.routeService.currentMeeting.subscribe(data => {
-      this.meeting = new MeetingImpl(data.meeting);
-      const body = document.getElementsByTagName("body").item(0);
-      if (this.meeting && this.meeting.meet_id) {
-        if (this.meeting.layout && this.meeting.layout.color_set && this.meeting.layout.color_set.primary && this.meeting.layout.color_set.secondary) {
-          body?.style.setProperty("--bg-gradient-1", this.meeting.layout.color_set.primary);
-          body?.style.setProperty("--bg-gradient-2", this.meeting.layout.color_set.secondary);
-        } else {
-          body?.style.setProperty("--bg-gradient-1", "#a3ffff");
-          body?.style.setProperty("--bg-gradient-2", "#ffa3ed");
+        let lang = window.localStorage.getItem("language");
+        if (!lang) {
+            //lang = navigator.language;
+            lang = "de";
         }
-      } else {
-        body?.style.removeProperty("--bg-gradient-1");
-        body?.style.removeProperty("--bg-gradient-2");
-      }
-    })
-    this.meetingIdSubscription = this.routeService.currentMeetingId.subscribe(data => {
-      this.meetingId = data;
-    })
-  }
+        this.translateService.use(lang);
 
-  ngOnDestroy() {
-    this.meetingSubscription.unsubscribe();
-    this.meetingIdSubscription.unsubscribe();
-  }
+        this.menuService.viewType.subscribe(data => {
+            this.sidebarState = data;
+        })
 
-  async fetchBuild() {
-    const response = await fetch("assets/release.txt");
-    return await response.text();
-  }
 
-  toggleBuild() {
-    this.showBuild = !this.showBuild;
-  }
+        this.meetingSubscription = this.routeService.currentMeeting.subscribe(data => {
+            this.meeting = new MeetingImpl(data.meeting);
+            const body = document.getElementsByTagName("body").item(0);
+            if (this.meeting && this.meeting.meet_id) {
 
-  toggleEnv() {
-    this.showEnv = !this.showEnv;
-  }
+                const title = this.meeting.getFullSeriesNameWithYear();
 
-  hideSidebar() {
-    this.menuService.setViewType("hidden");
-  }
+                this.headerTitle.setTitle(`${title} | SwimResults`);
+
+                this.translateService.get("META.DESCRIPTION", {meetingTitle: title}).subscribe({
+                    next: text => {
+                        this.headerMeta.updateTag({
+                            name: 'description', content: text
+                        });
+                    },
+                    error: _ => {
+                        this.headerMeta.updateTag({
+                            name: 'description',
+                            content: `Meldungen, Ergebnisse und Livetiming für '${title}' bei SwimResults`
+                        });
+                    }
+                })
+
+                if (this.meeting.layout && this.meeting.layout.color_set && this.meeting.layout.color_set.primary && this.meeting.layout.color_set.secondary) {
+                    body?.style.setProperty("--bg-gradient-1", this.meeting.layout.color_set.primary);
+                    body?.style.setProperty("--bg-gradient-2", this.meeting.layout.color_set.secondary);
+                } else {
+                    body?.style.setProperty("--bg-gradient-1", "#a3ffff");
+                    body?.style.setProperty("--bg-gradient-2", "#ffa3ed");
+                }
+            } else {
+                body?.style.removeProperty("--bg-gradient-1");
+                body?.style.removeProperty("--bg-gradient-2");
+            }
+        })
+        this.meetingIdSubscription = this.routeService.currentMeetingId.subscribe(data => {
+            this.meetingId = data;
+        })
+    }
+
+    ngOnDestroy() {
+        this.meetingSubscription.unsubscribe();
+        this.meetingIdSubscription.unsubscribe();
+    }
+
+    async fetchBuild() {
+        const response = await fetch("assets/release.txt");
+        return await response.text();
+    }
+
+    toggleBuild() {
+        this.showBuild = !this.showBuild;
+    }
+
+    toggleEnv() {
+        this.showEnv = !this.showEnv;
+    }
+
+    hideSidebar() {
+        this.menuService.setViewType("hidden");
+    }
 
     protected readonly MeetingStates = MeetingStates;
 }
