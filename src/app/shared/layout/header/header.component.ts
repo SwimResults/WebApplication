7051480006d65fb070e8ccdab3a89c775c8480fe
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-11-01 09:08:49 +0100

    add warnings to header

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -5,12 +5,13 @@ import {SidebarMenuService} from "../../../core/service/sidebar-menu.service";
 import {PanelComponent} from '../../elements/panel/panel.component';
 import {IconPanelComponent} from '../../elements/icon-panel/icon-panel.component';
 import {HeaderButtonsComponent} from './header-buttons/header-buttons.component';
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
     selector: 'sr-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
-    imports: [PanelComponent, IconPanelComponent, HeaderButtonsComponent]
+    imports: [PanelComponent, IconPanelComponent, HeaderButtonsComponent, MatIcon]
 })
 export class HeaderComponent {
   private routeService = inject(RouteService);
